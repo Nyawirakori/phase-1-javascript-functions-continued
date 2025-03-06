55c2191e9b7edb3f303a0ab23de6366fea9b9399
Joan Nyawira Kori <korijoan171@gmail.com>
2025-03-06 09:13:01 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+// // code your solution here
+function saturdayFun(activity = "roller-skate"){
+     return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+saturdayFun("bathe my dog!");
+
+//second function
+function mondayWork(activity = "go to the office"){
+     return `This Monday, I will ${activity}.`;
+}
+mondayWork();
+mondayWork("work from home");
+
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`;
+    };
+}
+
+
+console.log(wrapAdjective()()); 
+console.log(wrapAdjective("*")("a hard worker")); 
+console.log(wrapAdjective("||")("a dedicated programmer")); 
